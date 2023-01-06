14cb435078f2da4dfd1e953b544f3afea91d9be2
star <chlvkfksquf@gmail.com>
2023-01-06 16:25:41 +0900

    Add validation

--- src/components/pages/auth/Login.tsx
+++ src/components/pages/auth/Login.tsx
@@ -3,15 +3,25 @@ import React, { useState } from "react";
 import { login } from "api/auth";
 import getErrorMessage from "utils/error";
 
-interface inputState {
+interface InputState {
   email: string;
   password: string;
 }
 
+interface InputErrorState {
+  emailError: boolean;
+  passwordError: boolean;
+}
+
 function Login() {
-  const [inputs, setInputs] = useState<inputState>({
+  const [inputs, setInputs] = useState<InputState>({
     email: "",
     password: "",
+  });
+
+  const [inputsError, setInputsError] = useState<InputErrorState>({
+    emailError: false,
+    passwordError: false,
   });
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -24,38 +34,72 @@ function Login() {
   };
 
   const handleSubmit = async () => {
-    const { email, password } = inputs;
-    try {
-      const res = await login({ email, password });
+    if (!Object.values(inputsError).some((error) => error)) {
+      const { email, password } = inputs;
+      try {
+        const res = await login({ email, password });
 
-      alert(res.message);
+        alert(res.message);
 
-      window.localStorage.setItem("token", res.token);
-    } catch (error) {
-      const errorMessage = getErrorMessage(error);
+        window.localStorage.setItem("token", res.token);
+      } catch (error) {
+        const errorMessage = getErrorMessage(error);
 
-      alert(errorMessage.response.data.details);
+        alert(errorMessage.response.data.details);
+      }
+    } else {
+      alert("다시 확인해주세요.");
     }
   };
+  const checkEmail = (e: React.FocusEvent<HTMLInputElement>) => {
+    const regExp =
+      /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
 
+    setInputsError({
+      ...inputsError,
+      emailError: !regExp.test(e.target.value),
+    });
+  };
+
+  const checkPassword = () => {
+    const pwValid = inputs.password.length < 8;
+
+    setInputsError({ ...inputsError, passwordError: pwValid });
+  };
   return (
-    <div>
+    <div className="flex flex-col space-y-5">
       <input
         placeholder="이메일"
-        className="px-2 py-3 border border-gray-300"
+        className="px-2 py-3 border border-gray-300 rounded-md focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
         onChange={onChange}
         name="email"
         value={inputs.email}
+        onBlur={checkEmail}
       />
+      {inputsError.emailError && (
+        <span className="text-sm text-red-600" style={{ marginTop: 4 }}>
+          이메일 형식을 지켜주세요.
+        </span>
+      )}
       <input
         placeholder="비밀번호"
-        className="px-2 py-3 border border-gray-300"
+        className="px-2 py-3 border border-gray-300 rounded-md focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
         onChange={onChange}
         name="password"
         value={inputs.password}
+        onBlur={checkPassword}
       />
-
-      <button onClick={handleSubmit}>로그인</button>
+      {inputsError.passwordError && (
+        <span className="text-sm text-red-600" style={{ marginTop: 4 }}>
+          비밀번호는 8글자 이상 입력해 주세요.
+        </span>
+      )}
+      <button
+        onClick={handleSubmit}
+        className="h-12 text-white bg-indigo-500 rounded-md hover:bg-indigo-700"
+      >
+        로그인
+      </button>
     </div>
   );
 }
--- src/components/pages/auth/Signin.tsx
+++ src/components/pages/auth/Signin.tsx
@@ -8,12 +8,23 @@ interface inputState {
   password: string;
   checkPassword: string;
 }
+interface InputErrorState {
+  emailError: boolean;
+  passwordError: boolean;
+  checkPasswordError: boolean;
+}
 
 function Signin() {
   const [inputs, setInputs] = useState<inputState>({
     email: "",
     password: "",
     checkPassword: "",
+  });
+
+  const [inputsError, setInputsError] = useState<InputErrorState>({
+    emailError: false,
+    passwordError: false,
+    checkPasswordError: false,
   });
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -26,42 +37,92 @@ function Signin() {
   };
 
   const handleSubmit = async () => {
-    const { email, password } = inputs;
-    try {
-      const res = await signup({ email, password });
+    if (!Object.values(inputsError).some((error) => error)) {
+      const { email, password } = inputs;
+      try {
+        const res = await signup({ email, password });
 
-      alert(res.message);
-    } catch (error) {
-      const errorMessage = getErrorMessage(error);
+        alert(res.message);
+      } catch (error) {
+        const errorMessage = getErrorMessage(error);
 
-      alert(errorMessage.response.data.details);
+        alert(errorMessage.response.data.details);
+      }
+    } else {
+      alert("다시 확인해주세요.");
     }
   };
 
+  const checkEmail = (e: React.FocusEvent<HTMLInputElement>) => {
+    const regExp =
+      /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+
+    setInputsError({
+      ...inputsError,
+      emailError: !regExp.test(e.target.value),
+    });
+  };
+
+  const checkPassword = () => {
+    const pwValid = inputs.password.length < 8;
+
+    setInputsError({ ...inputsError, passwordError: pwValid });
+  };
+
+  const checkCheckPassword = () => {
+    const checkPwValid = inputs.password !== inputs.checkPassword;
+
+    setInputsError({ ...inputsError, checkPasswordError: checkPwValid });
+    console.log(inputsError.checkPasswordError);
+  };
+
   return (
-    <div>
+    <div className="flex flex-col space-y-5">
       <input
         placeholder="이메일"
-        className="px-2 py-3 border border-gray-300"
+        className="px-2 py-3 border border-gray-300 rounded-md focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
         onChange={onChange}
         name="email"
         value={inputs.email}
+        onBlur={checkEmail}
       />
+      {inputsError.emailError && (
+        <span className="text-sm text-red-600" style={{ marginTop: 4 }}>
+          이메일 형식을 지켜주세요.
+        </span>
+      )}
       <input
         placeholder="비밀번호"
-        className="px-2 py-3 border border-gray-300"
+        className="px-2 py-3 border border-gray-300 rounded-md focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
         onChange={onChange}
         name="password"
         value={inputs.password}
+        onBlur={checkPassword}
       />
+      {inputsError.passwordError && (
+        <span className="text-sm text-red-600" style={{ marginTop: 4 }}>
+          비밀번호는 8글자 이상 입력해 주세요.
+        </span>
+      )}
       <input
         placeholder="비밀번호 확인"
-        className="px-2 py-3 border border-gray-300"
+        className="px-2 py-3 border border-gray-300 rounded-md focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
         onChange={onChange}
         name="checkPassword"
         value={inputs.checkPassword}
+        onBlur={checkCheckPassword}
       />
-      <button onClick={handleSubmit}>회원가입</button>
+      {inputsError.checkPasswordError && (
+        <span className="text-sm text-red-600" style={{ marginTop: 4 }}>
+          비밀번호를 다시 확인해주세요.
+        </span>
+      )}
+      <button
+        onClick={handleSubmit}
+        className="h-12 text-white bg-indigo-500 rounded-md hover:bg-indigo-700"
+      >
+        회원가입
+      </button>
     </div>
   );
 }
