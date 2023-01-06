ad68e1e1675995093f1eec907ce0970e272465a5
star <chlvkfksquf@gmail.com>
2023-01-06 14:13:13 +0900

    Add Auth base

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,7 +1,44 @@
 import React from "react";
+import { Tab } from "@headlessui/react";
 
 function Auth() {
-  return <div>Auth</div>;
+  return (
+    <div className="min-h-full px-4 py-12 mx-auto w-96">
+      <Tab.Group>
+        <Tab.List>
+          <Tab>로그인</Tab>
+          <Tab>회원가입</Tab>
+        </Tab.List>
+        <Tab.Panels>
+          <Tab.Panel className="flex flex-col mx-auto">
+            <input
+              placeholder="이메일"
+              className="px-2 py-3 border border-gray-300"
+            />
+            <input
+              placeholder="비밀번호"
+              className="px-2 py-3 border border-gray-300"
+            />
+          </Tab.Panel>
+          <Tab.Panel className="flex flex-col">
+            <input
+              placeholder="이메일"
+              className="px-2 py-3 border border-gray-300"
+            />
+            <input
+              placeholder="비밀번호"
+              className="px-2 py-3 border border-gray-300"
+            />
+            <input
+              placeholder="비밀번호 확인"
+              className="px-2 py-3 border border-gray-300"
+            />
+            <button>회원가입</button>
+          </Tab.Panel>
+        </Tab.Panels>
+      </Tab.Group>
+    </div>
+  );
 }
 
 export default Auth;
