50e627f7d14007485eb430bfca0d93f48d64e4f2
star <chlvkfksquf@gmail.com>
2023-01-06 16:57:27 +0900

    Add style

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -6,11 +6,28 @@ import Signin from "components/pages/auth/Signin";
 
 function Auth() {
   return (
-    <div className="min-h-full px-4 py-12 mx-auto w-96">
+    <div
+      className="absolute transform w-96 top-1/2 left-1/2"
+      style={{
+        transform: "translate(-50%, -50%)",
+      }}
+    >
       <Tab.Group>
-        <Tab.List>
-          <Tab>로그인</Tab>
-          <Tab>회원가입</Tab>
+        <Tab.List className="text-center ">
+          <Tab
+            className={({ selected }) =>
+              selected ? "pt-5 pb-2 font-bold w-1/2" : "pt-5 pb-2 w-1/2"
+            }
+          >
+            로그인
+          </Tab>
+          <Tab
+            className={({ selected }) =>
+              selected ? "pt-5 pb-2 font-bold w-1/2" : "pt-5 pb-2 w-1/2"
+            }
+          >
+            회원가입
+          </Tab>
         </Tab.List>
         <Tab.Panels>
           <Tab.Panel className="flex flex-col">
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -24,8 +24,15 @@ function Main() {
         transform: "translate(-50%, -50%)",
       }}
     >
-      <div>TodoList</div>
-      <button onClick={goAuth}>로그인 / 회원가입 하러가기</button>
+      <div className="mt-6 text-3xl font-bold tracking-tight text-center text-gray-900">
+        TodoList
+      </div>
+      <button
+        className="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md group hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        onClick={goAuth}
+      >
+        로그인 / 회원가입 하러가기
+      </button>
     </div>
   );
 }
