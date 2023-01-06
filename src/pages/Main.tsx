2f6c7ea25f7ad1a49ba96bb97a45f9c6176d1f65
star <chlvkfksquf@gmail.com>
2023-01-06 14:13:25 +0900

    Add Main base

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
+import { useNavigate } from "react-router-dom";
+
 function Main() {
-  return <div>Main</div>;
+  const navigate = useNavigate();
+
+  const goAuth = () => {
+    navigate(`/auth`);
+  };
+
+  return (
+    <div
+      className="absolute transform w-96 top-1/2 left-1/2"
+      style={{
+        transform: "translate(-50%, -50%)",
+      }}
+    >
+      <div>TodoList</div>
+      <button onClick={goAuth}>로그인 / 회원가입 하러가기</button>
+    </div>
+  );
 }
 
 export default Main;
